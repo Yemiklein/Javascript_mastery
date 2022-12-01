f292bb97eac2e635414ff4d14ae662bad86d115a
Adeyemi <yemiklein77@gmail.com>
2022-12-01 20:18:13 +0100

    arrow function

---
+++ starter/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// function logger () {console.log('my name is yemicoder');}
+
+// logger();
+// logger();
+// logger();
+// logger();
+
+const calcAge = birthYear => 2037 - birthYear;
