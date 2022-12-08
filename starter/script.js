587d3350bcc926ba63121b451bc9564020d601d3
Adeyemi <yemiklein77@gmail.com>
2022-12-08 01:40:55 +0100

    array

--- starter/script.js
+++ starter/script.js
@@ -7,5 +7,24 @@
 // logger();
 // logger();
 
-const calcAge = birthYear => 2037 - birthYear;
-calcAge();
+// const calcAge = birthYear => 2037 - birthYear;
+// calcAge();
+//Add Element
+const friends = [ 'Dele', 'Kunle', 'Yemi', 'Bola', 'Tolu' ];
+const newFriends = friends.push('James', 'Bond')
+
+console.log(friends);
+console.log(newFriends);
+
+friends.unshift('Adeyemi', 'Raji');
+console.log(friends);
+
+//Remove Element
+
+friends.pop();
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf('Yemi'));
