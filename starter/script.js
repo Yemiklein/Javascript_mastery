5c68033eea898e81c3e5416f72e3bfef5ad57e1f
Adeyemi <yemiklein77@gmail.com>
2022-12-09 03:33:19 +0100

    if loop

--- starter/script.js
+++ starter/script.js
@@ -10,21 +10,44 @@
 // const calcAge = birthYear => 2037 - birthYear;
 // calcAge();
 //Add Element
-const friends = [ 'Dele', 'Kunle', 'Yemi', 'Bola', 'Tolu' ];
-const newFriends = friends.push('James', 'Bond')
+// const friends = [ 'Dele', 'Kunle', 'Yemi', 'Bola', 'Tolu' ];
+// const newFriends = friends.push('James', 'Bond')
 
-console.log(friends);
-console.log(newFriends);
+// console.log(friends);
+// console.log(newFriends);
 
-friends.unshift('Adeyemi', 'Raji');
-console.log(friends);
+// friends.unshift('Adeyemi', 'Raji');
+// console.log(friends);
 
-//Remove Element
+// //Remove Element
 
-friends.pop();
-console.log(friends);
+// friends.pop();
+// console.log(friends);
 
-friends.shift();
-console.log(friends);
+// friends.shift();
+// console.log(friends);
 
-console.log(friends.indexOf('Yemi'));
+// console.log(friends.indexOf('Yemi'));
+
+// const yemi = {
+//     firstName: 'Yemi',
+//     lastName: 'Adeyemi',
+//     email: 'yemiklein@yahoo.com',
+//     birthYear: 1988,
+//     job: 'Software Developer',
+//     friends: ['Dele', 'Kunle', 'Yemi', 'Bola', 'Tolu'],
+//     hasDriversLicense: true,
+
+//     //  calcAge: function (birthYear) {
+//     //     return 2022 - birthYear;
+
+//     calcAge: function () {
+//         return 2022 - this.birthYear;
+//     },
+// };
+
+// console.log(yemi.calcAge())
+//console.log(yemi['calcAge'](1988))
+
+
+for(let rep = 1; rep <= 10; rep++) {console.log(`lifting weights repetion ${rep}`)}
